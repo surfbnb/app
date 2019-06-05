e1be42a811fd0ae1c2d9918013904faf8a1ab063
Akshay Raje <akshay.raje@gmail.com>
2019-06-05 23:09:16 +0530

    Prettier, minor changes and dependencies update

--- App.js
+++ App.js
@@ -1,25 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import {createBottomTabNavigator, createStackNavigator, createSwitchNavigator, createAppContainer} from 'react-navigation';
+
 import Authentication from "./src/components/Authentication";
 import HomePage from "./src/components/HomePage";
 import Other from "./src/components/Other";
-//
-// const HomeStack = createStackNavigator({ Home: HomePage, Other: Other });
-// const OtherStack = createStackNavigator({ Home: HomePage, Other: Other });
-const AppStack = createBottomTabNavigator({ Home: HomePage, Other: Other });
-const AuthStack = createStackNavigator({ AuthScreen: Authentication });
+
+const AppTabNavigator = createBottomTabNavigator({
+    Home: HomePage,
+    Other: Other
+});
+const AuthStackNavigator = createStackNavigator({
+    AuthScreen: Authentication
+});
 
 export default createAppContainer(createSwitchNavigator(
   {
-    App: AppStack,
-    Auth: AuthStack,
+    App: AppTabNavigator,
+    Auth: AuthStackNavigator,
   },
   {
     initialRouteName: 'Auth',
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,1 @@
-export const apiRoot = "https://demo-mappy.stagingost.com/demo/api/1129/3213e2cfeed268d4ff0e067aa9f5f528d85bdf577e30e3a266f22556865db23a/";
+export const API_ROOT = "https://demo-mappy.stagingost.com/demo/api/1129/3213e2cfeed268d4ff0e067aa9f5f528d85bdf577e30e3a266f22556865db23a/";
