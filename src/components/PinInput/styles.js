d76cbd806cdd2d51708da995c73bf8e1722a8aa6
Preshita Shirke <preshita@ost.com>
2019-06-10 18:58:18 +0530

    minor fix

---
+++ src/components/PinInput/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export default (inlineStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    padding: 20,
+    marginTop: '40%'
+  }
+}));
