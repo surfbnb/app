57ec8b9f1121076e3761cdb72b3dc8b490546d37
Preshita Shirke <preshita@ost.com>
2019-06-10 20:26:58 +0530

    add activate user

---
+++ src/services/ActivateUser.js
@@ -0,0 +1,22 @@
+import AsyncStorage from '@react-native-community/async-storage';
+import { OstWalletSdk } from '@ostdotcom/ost-wallet-sdk-react-native';
+import ActivateUserWorkflow from './OstWalletCallbacks/ActivateUser';
+
+class ActivateUser {
+  activateUser(pin) {
+    AsyncStorage.getItem('user').then((user) => {
+      user = JSON.parse(user);
+      OstWalletSdk.activateUser(
+        user.user_details.user_id,
+        pin,
+        user.user_pin_salt,
+        86400,
+        '1000000000000000000',
+        new ActivateUserWorkflow(),
+        console.warn
+      );
+    });
+  }
+}
+
+export default new ActivateUser();
---
+++ src/services/OstWalletCallbacks/ActivateUser.js
@@ -0,0 +1,33 @@
+import AsyncStorage from '@react-native-community/async-storage';
+import { OstWalletWorkFlowCallback } from '@ostdotcom/ost-wallet-sdk-react-native';
+import { API_ROOT } from './../../constants';
+
+class ActivateUser extends OstWalletWorkFlowCallback {
+  constructor() {
+    super();
+  }
+
+  flowComplete(ostWorkflowContext, ostContextEntity) {
+    console.log('flowComplete ostWorkflowContext', ostWorkflowContext, 'ostContextEntity', ostContextEntity);
+    AsyncStorage.getItem('user').then((user) => {
+      user = JSON.parse(user);
+      if (user.user_details.status === 'CREATED') {
+        fetch(`${API_ROOT}/notify/user-activate`, {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        })
+          .then((response) => response.json())
+          .then((responseData) => {
+            console.log('user-activate responseData:', responseData);
+          })
+          .catch(console.warn)
+          .done();
+      }
+    });
+  }
+}
+
+export default ActivateUser;
