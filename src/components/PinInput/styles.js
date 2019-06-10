db5843eccd82d7eef0e1a6980fd0f680a68923e4
Shraddha <shraddha.patil@ost.com>
2019-06-10 23:13:58 +0530

    Revert "set pin component style"
    This reverts commit 91f229719d416f8812cf0341e5004116758b848b.

--- src/components/PinInput/styles.js
+++ src/components/PinInput/styles.js
@@ -1,18 +1,11 @@
 import { StyleSheet } from 'react-native';
 
-export default inlineStyles = StyleSheet.create({
+export default (inlineStyles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'flex-start',
     padding: 20,
-    marginTop: '10%'
-  },
-  displayText: {
-    marginBottom: 40,
-    marginHorizontal: 20,
-    textAlign: 'center',
-    color: '#101010',
-    fontSize: 16
-  },
-});
+    marginTop: '40%'
+  }
+}));
