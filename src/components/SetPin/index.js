713c0a6345e8ab5299363dbdb40d78a55e53363f
Shraddha <shraddha.patil@ost.com>
2019-06-12 16:49:25 +0530

    terms and privacy links to set pin

--- src/components/SetPin/index.js
+++ src/components/SetPin/index.js
@@ -13,13 +13,21 @@ export default class SetPin extends Component {
 
   render() {
     return (
-      <View style={{ marginTop: 25, paddingLeft: 50, paddingRight: 50 }}>
+      <View style={{ marginTop: 25,flex:1, paddingLeft: 50, paddingRight: 50 }}>
         <Text style={{textAlign: 'center', color: 'rgb(16, 16, 16)', fontSize: 15, lineHeight: 22, fontWeight: '300', marginBottom:20}}>
           Add a new 6-digit PIN to secure your Wallet. PIN will also help you recover the wallet if the phone is lost or stolen.
         </Text>
         <PinInput
           navigation={this.props.navigation}
           onPinChange={this.onPinChange}/>
+          <Text style={{flexDirection:'row', alignSelf:'center', marginBottom:5, fontSize:12, fontWeight:'300', color: 'rgb(136, 136, 136)' }}>
+           By Creating Your Wallet, you Agree to our
+        </Text>
+        <Text style={{flexDirection:'row', alignSelf:'center', marginBottom:15, fontSize:12, fontWeight:'500', color: 'rgb(136, 136, 136)' }}>
+          <Text onPress={() => Linking.openURL('http://google.com')}> Terms of Service </Text>
+          <Text style={{fontWeight:'300',}}>and</Text>
+          <Text onPress={() => Linking.openURL('http://google.com')}> Privacy Policy </Text>
+        </Text>
       </View>
     );
   }
