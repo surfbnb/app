069aaf3bdd3728494b945537e2b9de8496516dba
Preshita Shirke <preshita@ost.com>
2019-06-12 20:26:40 +0530

    minor fix

--- src/components/AuthLoading/index.js
+++ src/components/AuthLoading/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { View, ActivityIndicator, StatusBar } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
-import deepGet from "lodash/get"
+import deepGet from 'lodash/get';
+import isEmpty from 'lodash/isEmpty';
 
 import styles from './styles';
 
-//TODO move to constants 
+//TODO move to constants
 const userStatus = {
   activated: 'activated'
 };
@@ -19,14 +20,14 @@ export default class AuthLoading extends Component {
   // Fetch the token from storage then navigate to our appropriate place
   init = async () => {
     let user = await AsyncStorage.getItem('user');
-    user = JSON.parse( user ) || {} ;
+    user = JSON.parse(user) || {};
 
     // This will switch to the Home screen or SetPinScreen or Auth screen and this loading
     // screen will be unmounted and thrown away.
-    const status = deepGet(user ,  "user_details.ost_status") || '';
-    if (status.toLowerCase() !== userStatus.activated) {
+    const status = deepGet(user, 'user_details.ost_status') || '';
+    if (!isEmpty(user) && status.toLowerCase() !== userStatus.activated) {
       this.props.navigation.navigate('SetPinScreen');
-    } else if (user) {
+    } else if (!isEmpty(user)) {
       this.props.navigation.navigate('HomeScreen');
     } else {
       this.props.navigation.navigate('AuthScreen');
