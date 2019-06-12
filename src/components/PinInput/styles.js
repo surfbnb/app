a927ce7068b056be4d3aeb9850c652df571e7a83
thahir-reddy <thahir@ost.com>
2019-06-12 15:45:56 +0530

    UI

--- src/components/PinInput/styles.js
+++ src/components/PinInput/styles.js
@@ -1,11 +1,28 @@
-import { StyleSheet } from 'react-native';
+import DefaultStyleGenerator from "../../theme/styles/DefaultStyleGenerator";
 
-export default inlineStyles = StyleSheet.create({
+let stylesMap = {
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'flex-start',
     padding: 20,
-  },
-});
-//TODO remove this comment
+  }//,
+  // smoothPinCodeInput : {
+  //   marginTop: -10,
+  //   marginLeft: -1
+  // },
+  // ios: {
+  //   smoothPinCodeInput : {
+  //     marginTop: -10,
+  //     marginLeft: -1
+  //   }
+  // },
+  // android: {
+  //   smoothPinCodeInput: {
+  //     marginTop: -5,
+  //     marginLeft: -2.5
+  //   }
+  // }
+};
+
+export default styles = DefaultStyleGenerator.generate(stylesMap);
