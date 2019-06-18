4b37ed58bd4e95d6948f19aaef1fb3b6f6c1015d
Ashutosh <ashutosh@ost.com>
2019-06-18 11:51:54 +0530

    Profile screen

--- src/components/Profile/ProfileScreen.js
+++ src/components/Profile/ProfileScreen.js
@@ -10,6 +10,10 @@ import styles from './styles';
 class ProfileScreen extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      feeds : [],
+      refreshing : false
+    }
   }
 
   componentWillMount() {
@@ -34,13 +38,27 @@ class ProfileScreen extends Component {
       })
   };
 
+  onRefresh(){
+    this.setState({ refreshing  : true  });
+    this.fetchComponent
+    .refresh()
+    .then( ( res) => {
+      this.setState({ refreshing  : false , feeds : this.fetchComponent.getIDList() });
+    })
+    .catch((error)=>{
+      this.setState({ refreshing  : false  });
+    });
+  }
+
   render() {
-    if (this.props.user_feed && this.props.user_feed.length > 0) {
+    if (this.state.feeds && this.state.feeds.length > 0) {
       return (
         <View style={styles.container}>
           <FlatList
-            data={this.props.user_feed}
+            data={this.state.feeds}
             onEndReached={this.getFeedList}
+            onRefresh={()=>{this.onRefresh()}}
+            refreshing={this.state.refreshing}
             keyExtractor={(item, index) => `id_${item}`}
             onEndReachedThreshold={0.5}
             initialNumToRender={20}
