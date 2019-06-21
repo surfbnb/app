ddbc58476e42ccc08ea3159e42cf1b01079c3d1b
thahir-reddy <thahir@ost.com>
2019-06-21 13:54:25 +0530

    UI

--- src/components/Transaction/TransactionScreen.js
+++ src/components/Transaction/TransactionScreen.js
@@ -41,7 +41,7 @@ class TransactionScreen extends Component {
     return {
       headerTitle: navigation.getParam('transactionHeader'),
       headerBackImage: (
-        <View style={{ paddingRight: 30, paddingVertical: 30 }}>
+        <View style={{ paddingRight: 30, paddingVertical: 30, paddingLeft: Platform.OS === 'ios' ? 20 : 0 }}>
           <Image source={BackArrow} style={{ width: 10, height: 18, paddingLeft: 8 }} />
         </View>
       )
