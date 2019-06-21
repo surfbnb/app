a8115e7d1be4d7269d379d14b483c54235fd0b64
Akshay Raje <akshay.raje@gmail.com>
2019-06-21 13:32:19 +0530

    Prettier

--- src/services/Pricer.js
+++ src/services/Pricer.js
@@ -1,60 +1,63 @@
 import { OstWalletSdk, OstJsonApi } from '@ostdotcom/ost-wallet-sdk-react-native';
-import deepGet from "lodash/get";
+import deepGet from 'lodash/get';
 import { TOKEN_ID } from '../constants';
 
 class Pricer {
+  constructor() {
+    this.token = null;
+    this.pricePoints = null;
+  }
 
-    constructor(){
-        this.token = null ; 
-        this.pricePoints = null; 
+  getToken(successCallback) {
+    if (this.token) {
+      successCallback && successCallback(this.token);
+      return;
     }
+    OstWalletSdk.getToken(TOKEN_ID, (token) => {
+      this.token = token;
+      successCallback && successCallback(token);
+    });
+  }
 
-    getToken( successCallback ){
-        if(this.token){
-            successCallback && successCallback( this.token );
-            return;
-        }
-        OstWalletSdk.getToken(TOKEN_ID, (token) => {
-            this.token = token ; 
-            successCallback && successCallback( token );
+  getPricePoints(ostUserId, successCallback, errorCallback) {
+    let isCb = true;
+    if (!ostUserId) {
+      errorCallback &&
+        errorCallback({
+          success: false,
+          msg: 'No user found'
         });
+      return;
     }
+    if (this.pricePoints) {
+      successCallback && successCallback(this.pricePoints);
+      isCb = false;
+    }
+    OstJsonApi.getPricePointForUserId(
+      ostUserId,
+      (res) => {
+        this.pricePoints = deepGet(res, 'price_point.OST');
+        isCb && successCallback && successCallback(this.pricePoints);
+      },
+      (error) => {
+        isCb && errorCallback && errorCallback(error);
+      }
+    );
+  }
 
-    getPricePoints(ostUserId , successCallback , errorCallback ){
-        let isCb = true ; 
-        if( !ostUserId ){
-            errorCallback && errorCallback({
-                success: false , 
-                msg: "No user found"
-            });
-            return ;
+  getPriceOracleConfig(ostUserId, successCallback, errorCallback) {
+    this.getToken((token) => {
+      this.getPricePoints(
+        ostUserId,
+        (pricePoints) => {
+          successCallback && successCallback(token, pricePoints);
+        },
+        (error) => {
+          errorCallback && errorCallback(error);
         }
-        if(this.pricePoints){
-            successCallback && successCallback(this.pricePoints);
-            isCb = false ;
-        }
-        OstJsonApi.getPricePointForUserId(
-            ostUserId,
-            (res) => {
-              this.pricePoints = deepGet( res , "price_point.OST") ; 
-              isCb && successCallback && successCallback(this.pricePoints);
-            },
-            (error) => {
-              isCb && errorCallback && errorCallback(error);
-            }
-        );
-    }
-
-    getPriceOracleConfig(ostUserId ,  successCallback , errorCallback ){
-        this.getToken((token)=>{
-            this.getPricePoints(ostUserId , (pricePoints)=>{
-                successCallback && successCallback( token , pricePoints );
-            } , (error) => {
-                errorCallback && errorCallback( error );
-            });
-        });
-    }
-
+      );
+    });
+  }
 }
 
 export default new Pricer();
