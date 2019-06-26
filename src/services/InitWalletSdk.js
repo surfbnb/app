7c285f7ef9ce6e5fde6b7903ee61a7aae465ef5c
Akshay Raje <akshay.raje@gmail.com>
2019-06-26 20:04:46 +0530

    Minor enhancements

--- src/services/InitWalletSdk.js
+++ src/services/InitWalletSdk.js
@@ -2,7 +2,7 @@ import { OstWalletSdk } from '@ostdotcom/ost-wallet-sdk-react-native';
 import { TOKEN_ID } from '../constants';
 import SetupDeviceWorkflow from './OstWalletCallbacks/SetupDeviceWorkflow';
 import currenUserModel from '../models/CurrentUser';
-import { PLATFROM_API_ENDPOINT } from '../constants';
+import { PLATFORM_API_ENDPOINT } from '../constants';
 
 class InitWalletSdk {
   initializeDevice = (setupDeviceDelegate) => {
