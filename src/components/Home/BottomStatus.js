975d5f139c277dce2e761fbed7f4438c2e765136
thahir-reddy <thahir@ost.com>
2019-07-08 21:05:21 +0530

    home UI

--- src/components/Home/BottomStatus.js
+++ src/components/Home/BottomStatus.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
-import {View, Text, Image, TouchableOpacity} from "react-native";
-import TouchableButton from '../../theme/components/TouchableButton';
+import {View, Text, Image, TouchableOpacity, Alert} from "react-native";
+// import TouchableButton from '../../theme/components/TouchableButton';
 import Theme from '../../theme/styles';
 
 import inlineStyles from "./styles";
@@ -25,38 +25,46 @@ class BottomStatus extends PureComponent {
     });
   }
 
+  showAlert(e){
+    Alert.alert(
+      'YET TO BE IMFLEMENTED!'
+    )
+  }
+
   render(){
     return (
-      <View style={{ width: '100%', position: 'absolute', bottom: 50}}>
+      <View style={ inlineStyles.bottomContainer }>
         {/*<TouchableButton*/}
           {/*TouchableStyles={[Theme.Button.btnPink]}*/}
           {/*TextStyles={[Theme.Button.btnPinkText]}*/}
           {/*text="Transaction"*/}
           {/*onPress={(e) => {this.exTransaction(e)}}*/}
         {/*></TouchableButton>*/}
-        <View style={{alignSelf: 'flex-end', marginBottom: 15, marginRight: 20}}>
-          <TouchableOpacity onPress={(e) => {this.exTransaction(e)}}>
+        <View style={inlineStyles.pepoElem}>
+          <TouchableOpacity onPress={(e) => {this.showAlert(e)}}>
             <Image
               style={{height: 53, width: 53}}
               source={pepo_tx_icon}
             />
           </TouchableOpacity>
-          <Text style={{fontSize: 18, color: 'white', alignSelf: 'center', marginTop: 5}}>304</Text>
+          <Text style={inlineStyles.pepoTxCount}>304</Text>
         </View>
-        <TouchableOpacity onPress={(e) => {this.exTransaction(e)}}>
-          <Image
-            style={{marginBottom: 20, alignSelf: 'flex-end', marginRight: 20, height: 57, width: 57}}
-            source={tx_icon}
-          />
-        </TouchableOpacity>
-        <View style={{backgroundColor: 'rgba(0, 0, 0, 0.6)', maxHeight: 150, flex: 1, flexDirection: 'row', alignItems: 'center', paddingVertical: 15, paddingHorizontal: 15}}>
+        <View style={inlineStyles.txElem}>
+          <TouchableOpacity onPress={(e) => {this.exTransaction(e)}}>
+            <Image
+              style={{ height: 57, width: 57}}
+              source={tx_icon}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={inlineStyles.bottomBg}>
           <View style={{flex: 0.7, flexWrap: 'wrap'}}>
-            <Text style={{color: 'white', marginBottom: 5}}>@Annik</Text>
-            <Text style={{color: 'white', paddingRight: 20, fontSize: 13}}>Based out of NYC, I Play bass with ‘City of suns’.  You can find us at Atla - East Village every Saturday. #Podcaster #Artist #Musician</Text>
+            <Text style={[{marginBottom: 5}, inlineStyles.bottomBgTxt]}>@Annik</Text>
+            <Text style={[{paddingRight: 20, fontSize: 13}, inlineStyles.bottomBgTxt]}>Based out of NYC, I Play bass with ‘City of suns’.  You can find us at Atla - East Village every Saturday. #Podcaster #Artist #Musician</Text>
           </View>
           <View style={{flex: 0.3}}>
-            <Text style={{color: 'white', marginBottom: 5}}>$ 5K Raised</Text>
-            <Text style={{color: 'white'}}>$ 2K Supporters</Text>
+            <Text style={[{marginBottom: 5}, inlineStyles.bottomBgTxt]}>$ 5K Raised</Text>
+            <Text style={inlineStyles.bottomBgTxt}>$ 2K Supporters</Text>
           </View>
         </View>
       </View>
--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -1,7 +1,8 @@
-import { Dimensions  } from 'react-native';
+import DefaultStyleGenerator from '../../theme/styles/DefaultStyleGenerator';
+import { Dimensions } from 'react-native';
+import Colors from '../../theme/styles/Colors';
 
 let stylesMap = {
-    
     fullScreen: {
         width: Dimensions.get('screen').width,
         height: Dimensions.get('screen').height
@@ -9,8 +10,37 @@ let stylesMap = {
     fullHeight: {
         width: '100%',
         height: Dimensions.get('screen').height
-    }
-
+    },
+    pepoElem: {
+        alignSelf: 'flex-end', marginBottom: 15, marginRight: 20
+    },
+    pepoTxCount:{
+        fontSize: 18,
+        color: 'white',
+        alignSelf: 'center',
+        marginTop: 5
+    },
+    txElem: {
+        marginBottom: 20,
+        alignSelf: 'flex-end',
+        marginRight: 20,
+    },
+      bottomContainer: {
+        width: '100%',
+        position: 'absolute',
+        bottom: 50
+      },
+  bottomBg: {
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    maxHeight: 150,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 15,
+    paddingHorizontal: 15
+  },
+  bottomBgTxt:{
+        color: 'white'
+  }
 };
-  
-  export default stylesMap;
+
+export default styles = DefaultStyleGenerator.generate(stylesMap);
