7c8dc96f6adbd340dc9e86579a0364a85e6bc326
thahir-reddy <thahir@ost.com>
2019-07-10 11:42:07 +0530

    UA UI

--- src/components/UserActivating/index.js
+++ src/components/UserActivating/index.js
@@ -1,11 +1,11 @@
-//import liraries
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-import Theme from '../../../theme/styles';
+import TouchableButton from '../../theme/components/TouchableButton';
+import Theme from '../../theme/styles';
 
 // create a component
-class UserActivatingScreen extends Component {
+export default class UserActivatingScreen extends Component {
   onCreatePin() {
     this.props.navigation.navigate('SetPinScreen');
   }
@@ -32,9 +32,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#2c3e50'
+    backgroundColor: '#fff'
   }
 });
-
-//make this component available to the app
-export default UserActivatingScreen;
