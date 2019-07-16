764493c6a3f8eb392088d123b10b82667399c293
Shraddha <shraddha.patil@ost.com>
2019-07-16 21:28:56 +0530

    cover Image component

---
+++ src/components/CommonComponents/CoverImage/styles.js
@@ -0,0 +1,19 @@
+import DefaultStyleGenerator from '../../../theme/styles/DefaultStyleGenerator';
+import Colors from '../../../theme/styles/Colors';
+import {Dimensions} from "react-native";
+
+let stylesMap = {
+  videoWrapperSkipFont:{
+    height: Dimensions.get('window').height * .60
+  },
+  playIconSkipFont :{
+    position:'absolute',
+    height:25,
+    width:25,
+    top:Dimensions.get('window').height * 0.65 * 0.50 -12 ,
+    left:Dimensions.get('window').width * 0.50 -12
+  }
+
+};
+
+export default styles = DefaultStyleGenerator.generate(stylesMap);
