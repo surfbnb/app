16bbfb96d057dbf8297900b2475aa2009250a087
thahir-reddy <thahir@ost.com>
2019-07-17 16:59:55 +0530

    Balance header UI

--- src/components/Profile/BalanceHeader.js
+++ src/components/Profile/BalanceHeader.js
@@ -4,6 +4,8 @@ import { View, Text, Image } from 'react-native';
 import pricer from '../../services/Pricer';
 import inlineStyles from './styles';
 import utilities from '../../services/Utilities';
+import profilelWallet from '../../assets/profile-wallet-icon.png'
+import pepoWhiteIcon from '../../assets/pepo-white-icon.png'
 
 const mapStateToProps = (state) => ({ balance: state.balance });
 
@@ -28,12 +30,18 @@ class BalanceHeader extends PureComponent {
   render() {
     return (
         <View style={inlineStyles.balanceHeader}>
-          <Text style={inlineStyles.balanceToptext}>Your Balance</Text>
-          <Text style={inlineStyles.pepoBalance}>
-            <Image style={{ width: 25, height: 22 }} source={utilities.getTokenSymbolImageConfig()['image2']}></Image>{' '}
-            {this.toBt(this.props.balance)}
-          </Text>
-          <Text style={inlineStyles.usdBalance}>$ {this.toFiat( this.props.balance )} </Text>
+          <View style={{flex: 0.5, flexDirection: 'row', alignItems: 'center'}}>
+            <Image style={{ width: 36.6, height: 36.6 }} source={profilelWallet}></Image>
+            <Text style={inlineStyles.balanceToptext}>Balance</Text>
+          </View>
+          <View style={{flex: 0.5, alignItems: 'flex-end', justifyContent: 'flex-end'}}>
+            <Text style={inlineStyles.pepoBalance}>
+              {/*<Image style={{ width: 25, height: 22 }} source={utilities.getTokenSymbolImageConfig()['image2']}></Image>{' '}*/}
+              <Image style={{ width: 18, height: 18 }} source={pepoWhiteIcon}></Image>
+              {this.toBt(this.props.balance)}
+            </Text>
+            <Text style={inlineStyles.usdBalance}>$ {this.toFiat( this.props.balance )} </Text>
+          </View>
         </View>
     );
   }
