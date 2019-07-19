ac7127ae965b8f4cd1f58f3ec74ad995f5f9b4ab
Preshita Shirke <preshita@ost.com>
2019-07-19 22:29:20 +0530

    camera view overlay done

--- src/components/CaptureImage/index.js
+++ src/components/CaptureImage/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Platform } from 'react-native';
+import { Platform, Image, TouchableWithoutFeedback, View, SafeAreaView } from 'react-native';
 
 import SnapClicker from '../SnapClicker';
 import CropperUI from '../ImageCropper/CropperUI';
@@ -9,6 +9,7 @@ import RNFS from 'react-native-fs';
 import CurrentUser from '../../models/CurrentUser';
 import PepoApi from '../../services/PepoApi';
 import appConfig from '../../constants/AppConfig';
+import tickIcon from '../../assets/tick_icon.png';
 
 class CaptureImage extends Component {
   constructor(props) {
@@ -17,17 +18,47 @@ class CaptureImage extends Component {
       imageURI: '',
       imageCaptured: false
     };
+    this.cropperRef = null;
   }
+
+  componentWillUnmount() {
+    this.cropperRef = null;
+  }
+
+  cropImage = () => {
+    if (this.cropperRef) {
+      this.cropperRef.cropImage();
+    }
+  };
 
   getView() {
     if (this.state.imageCaptured) {
       return (
-        <CropperUI
-          imageUri={this.state.imageURI}
-          screenHeightRatio={1}
-          onCrop={this.getCroppedImage}
-          onClose={this.closeCropper}
-        />
+        <SafeAreaView style={{ flex: 1 }}>
+          <View style={{ position: 'relative', flex: 0.6 }}>
+            <CropperUI
+              ref={(ref) => (this.cropperRef = ref)}
+              imageUri={this.state.imageURI}
+              screenHeightRatio={0.6}
+              onCrop={this.getCroppedImage}
+              onClose={this.closeCropper}
+            />
+          </View>
+          <View style={{ flex: 0.4, backgroundColor: '#000000' }}>
+            <TouchableWithoutFeedback onPress={this.cropImage}>
+              <Image
+                source={tickIcon}
+                style={{
+                  position: 'absolute',
+                  bottom: 22,
+                  right: 22,
+                  width: 45,
+                  height: 45
+                }}
+              />
+            </TouchableWithoutFeedback>
+          </View>
+        </SafeAreaView>
       );
     } else {
       return this.getCameraView();
