c58e624eb3766ab9e29dd1a8876c00f62dbc7040
thahir-reddy <thahir@ost.com>
2019-07-29 14:37:26 +0530

    Fragment instead of View

--- src/components/PinInput/PinFooter.js
+++ src/components/PinInput/PinFooter.js
@@ -6,9 +6,7 @@ export default () => {
   const keyboardVerticalOffset = Platform.OS === 'ios' ? 100 : -500;
   return (
     <KeyboardAvoidingView behavior="padding" keyboardVerticalOffset={keyboardVerticalOffset}>
-      <View>
-        <Text style={inlineStyles.termsPoliciesInfoText}>By Creating Your Wallet, you Agree to our</Text>
-      </View>
+      <Text style={inlineStyles.termsPoliciesInfoText}>By Creating Your Wallet, you Agree to our</Text>
       <Text style={inlineStyles.termsPoliciesLinkText}>
         <Text onPress={() => Alert.alert('todo: TOS link')}> Terms of Service </Text>
         <Text style={{ fontWeight: '300', color: '#484848' }}>and</Text>
