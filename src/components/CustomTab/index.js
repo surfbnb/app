93c4ce451bd93d44b60458eb0ffa4feaf35162cc
thahir-reddy <thahir@ost.com>
2019-08-06 22:12:28 +0530

    custom component UI

--- src/components/CustomTab/index.js
+++ src/components/CustomTab/index.js
@@ -4,6 +4,16 @@ import { StackActions, NavigationActions, SafeAreaView } from 'react-navigation'
 
 import styles from './styles';
 import feed from '../../assets/user_feed.png';
+import homeNs from '../../assets/user-home-icon.png';
+import homeSelected from '../../assets/user-home-icon-selected.png';
+import profileNs from '../../assets/user-profile-icon.png';
+import profileSelected from '../../assets/user-profile-icon-selected.png';
+import searchNs from '../../assets/user-search-icon.png';
+import searchSelected from '../../assets/user-search-icon-selected.png';
+import activityNs from '../../assets/user-activity-icon.png';
+import activitySelected from '../../assets/user-activity-icon-selected.png';
+import videoNs from '../../assets/user-video-capture-icon.png';
+import videoSelected from '../../assets/user-video-capture-icon-selected.png';
 import global from '../../assets/user_global.png';
 import profile from '../../assets/user_profile.png';
 import friends from '../../assets/user_friends.png';
@@ -65,46 +75,56 @@ function onTabPressed(navigation, tab) {
 }
 
 const CustomTab = ({ navigation, screenProps }) => {
-  previousTabIndex = navigation.state.index 
+  previousTabIndex = navigation.state.index
    return ( <SafeAreaView forceInset={{ top: 'never' }} style={styles.container}>
       <TouchableOpacity onPress={() => onTabPressed(navigation, tabConfig.tab1)}>
         <Image
-          tintColor={navigation.state.index === tabConfig.tab1.index ? '#61b2d6' : 'rgb(72,72,72)'}
+          // tintColor={navigation.state.index === tabConfig.tab1.index ? '#61b2d6' : 'rgb(72,72,72)'}
           style={[
-            styles.tabElementSkipFont,
-            { tintColor: navigation.state.index === tabConfig.tab1.index ? '#ef5566' : '#484848' }
+            styles.tabElementSkipFont
+            // { tintColor: navigation.state.index === tabConfig.tab1.index ? '#ef5566' : '#484848' }
           ]}
-          source={global}
+          source={navigation.state.index === tabConfig.tab1.index ? homeNs : homeSelected}
         />
       </TouchableOpacity>
       <TouchableOpacity onPress={() => onTabPressed(navigation, tabConfig.tab2)}>
         <Image
-          tintColor={navigation.state.index === tabConfig.tab2.index ? '#61b2d6' : 'rgb(72,72,72)'}
+          // tintColor={navigation.state.index === tabConfig.tab2.index ? '#61b2d6' : 'rgb(72,72,72)'}
           style={[
-            { height: 24, width: 28 },
-            { tintColor: navigation.state.index === tabConfig.tab2.index ? '#ef5566' : '#484848' }
+            styles.tabElementSkipFont
+            // { tintColor: navigation.state.index === tabConfig.tab2.index ? '#ef5566' : '#484848' }
           ]}
-          source={feed}
+          source={navigation.state.index === tabConfig.tab2.index ? searchNs : searchSelected}
         />
       </TouchableOpacity>
+     <TouchableOpacity>
+       <Image
+         // tintColor={navigation.state.index === tabConfig.tab2.index ? '#61b2d6' : 'rgb(72,72,72)'}
+         style={[
+           styles.tabElementSkipFont
+           // { tintColor: navigation.state.index === tabConfig.tab2.index ? '#ef5566' : '#484848' }
+         ]}
+         source={videoNs}
+       />
+     </TouchableOpacity>
       <TouchableOpacity onPress={() => onTabPressed(navigation, tabConfig.tab3)}>
         <Image
-          tintColor={navigation.state.index === tabConfig.tab3.index ? '#61b2d6' : '#484848'}
+          // tintColor={navigation.state.index === tabConfig.tab3.index ? '#61b2d6' : '#484848'}
           style={[
-            styles.tabElementFriendsSkipFont,
-            { tintColor: navigation.state.index === tabConfig.tab3.index ? '#ef5566' : '#484848' }
+            styles.tabElementSkipFont
+            // { tintColor: navigation.state.index === tabConfig.tab3.index ? '#ef5566' : '#484848' }
           ]}
-          source={friends}
+          source={navigation.state.index === tabConfig.tab3.index ? activityNs : activitySelected}
         />
       </TouchableOpacity>
       <TouchableOpacity onPress={() => onTabPressed(navigation, tabConfig.tab4)}>
         <Image
-          tintColor={navigation.state.index === tabConfig.tab4.index ? '#61b2d6' : '#484848'}
+          // tintColor={navigation.state.index === tabConfig.tab4.index ? '#61b2d6' : '#484848'}
           style={[
-            styles.tabElementSkipFont,
-            { tintColor: navigation.state.index === tabConfig.tab4.index ? '#ef5566' : '#484848' }
+            styles.tabElementSkipFont
+            // { tintColor: navigation.state.index === tabConfig.tab4.index ? '#ef5566' : '#484848' }
           ]}
-          source={profile}
+          source={navigation.state.index === tabConfig.tab4.index ? profileNs : profileSelected}
         />
       </TouchableOpacity>
     </SafeAreaView>)
