91c8aa926bba9cce4662b47ccba652b0c1cbb044
thahir-reddy <thahir@ost.com>
2019-08-16 00:52:14 +0530

    removing updates text from user history videos

--- src/components/UsersProfile/styles.js
+++ src/components/UsersProfile/styles.js
@@ -2,16 +2,16 @@ import DefaultStyleGenerator from '../../theme/styles/DefaultStyleGenerator';
 
 let stylesMap = {
 
-  updates: {
-    textAlign: 'center',
-    borderColor: 'rgb(218, 223, 220)',
-    borderWidth: 1,
-    color: '#2a293b',
-    fontSize: 18,
-    fontFamily: 'AvenirNext-Regular',
-    paddingVertical: 10,
-    marginTop: 30
-  },
+  // updates: {
+  //   textAlign: 'center',
+  //   borderColor: 'rgb(218, 223, 220)',
+  //   borderWidth: 1,
+  //   color: '#2a293b',
+  //   fontSize: 18,
+  //   fontFamily: 'AvenirNext-Regular',
+  //   paddingVertical: 10,
+  //   marginTop: 30
+  // },
 
 };
 
