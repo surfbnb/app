1611590cf7dab04c888fb959192656245c2608d3
Akshay Raje <akshay.raje@gmail.com>
2019-08-17 23:52:02 +0530

    Logout and Twitter disconnect fix

--- src/components/CustomDrawerContent/index.js
+++ src/components/CustomDrawerContent/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, ScrollView, TouchableOpacity, Text, View, Image, Alert } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, ScrollView, TouchableOpacity, Text, View, Image } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import CurrentUser from '../../models/CurrentUser';
 import reduxGetter from '../../services/ReduxGetters';
@@ -7,66 +7,97 @@ import PepoApi from '../../services/PepoApi';
 import Colors from '../../theme/styles/Colors';
 import loggedOutIcon from '../../assets/drawer-logout-icon.png';
 import twitterDisconnectIcon from '../../assets/drawer-twitter-icon.png';
+import Toast from '../NotificationToast';
 
 import BackArrow from '../../assets/back-arrow.png';
 
-function twitterDisconnect() {
-  new PepoApi('/auth/twitter-disconnect')
-    .post()
-    .catch((error) => {
-      Alert.alert('Twitter Disconnect failed', error);
-    })
-    .then(async (res) => {
-      console.log('Twitter disconnect', res);
-      if (res && res.success) {
-        await CurrentUser.logout();
-      } else {
-        Alert.alert('Twitter Disconnect failed', res);
-      }
-    });
-}
+export default class CustomDrawerContent extends Component{
 
-let userName;
-export default (props) => {
-  let currentUserName = reduxGetter.getName(CurrentUser.getUserId());
-  userName = currentUserName || userName;
-  return (
-    <ScrollView style={styles.container}>
-      <SafeAreaView forceInset={{ top: 'always' }}>
-        <View
-          style={{
-            paddingVertical: 10,
-            borderBottomColor: Colors.seaMist,
-            borderBottomWidth: 1,
-            flexDirection: 'row',
-            alignItems: 'center',
-            paddingHorizontal: 5,
-            justifyContent: 'center'
-          }}
-        >
-          <TouchableOpacity
-            onPress={props.navigation.closeDrawer}
-            style={{ height: 30, width: 30, alignItems: 'center', justifyContent: 'center' }}
-          >
-            <Image style={{ width: 10, height: 18 }} source={BackArrow} />
-          </TouchableOpacity>
-          <Text style={styles.headerText}>{userName}</Text>
-        </View>
-        <TouchableOpacity onPress={twitterDisconnect}>
-          <View style={styles.itemParent}>
-            <Image style={{ height: 24, width: 25.3 }} source={twitterDisconnectIcon} />
-            <Text style={styles.item}>Twitter Disconnect</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={CurrentUser.logout.bind(CurrentUser)}>
-          <View style={styles.itemParent}>
-            <Image style={{ height: 24, width: 25.3 }} source={loggedOutIcon} />
-            <Text style={styles.item}>Log out</Text>
-          </View>
-        </TouchableOpacity>
-      </SafeAreaView>
-    </ScrollView>
-  );
+  constructor() {
+    super();
+    this.userName = reduxGetter.getName(CurrentUser.getUserId());
+    this.state = {
+      disableButtons: false
+    };
+    this.twitterDisconnect = this.twitterDisconnect.bind(this);
+    this.CurrentUserLogout = this.CurrentUserLogout.bind(this);
+  }
+
+  twitterDisconnect() {
+    this.setState({
+      disableButtons: true
+    });
+    new PepoApi('/auth/twitter-disconnect')
+        .post()
+        .catch((error) => {
+          Toast.show({
+            text: 'Twitter Disconnect failed',
+            icon: 'error'
+          });
+        })
+        .then(async (res) => {
+          if (res && res.success) {
+            await this.CurrentUserLogout();
+          } else {
+            Toast.show({
+              text: 'Twitter Disconnect failed',
+              icon: 'error'
+            });
+          }
+        })
+        .finally(() => this.setState({disableButtons: false}))
+  }
+
+  async CurrentUserLogout(){
+    this.setState({
+      disableButtons: true
+    });
+    await CurrentUser.logout();
+    this.setState({
+      disableButtons: false
+    });
+  }
+
+  render(){
+    return (
+        <ScrollView style={styles.container}>
+          <SafeAreaView forceInset={{ top: 'always' }}>
+            <View
+                style={{
+                  paddingVertical: 10,
+                  borderBottomColor: Colors.seaMist,
+                  borderBottomWidth: 1,
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  paddingHorizontal: 5,
+                  justifyContent: 'center'
+                }}
+            >
+              <TouchableOpacity
+                  onPress={this.props.navigation.closeDrawer}
+                  style={{ height: 30, width: 30, alignItems: 'center', justifyContent: 'center' }}
+              >
+                <Image style={{ width: 10, height: 18 }} source={BackArrow} />
+              </TouchableOpacity>
+              <Text style={styles.headerText}>{this.userName}</Text>
+            </View>
+            <TouchableOpacity onPress={this.twitterDisconnect} disabled={this.state.disableButtons}>
+              <View style={styles.itemParent}>
+                <Image style={{ height: 24, width: 25.3 }} source={twitterDisconnectIcon} />
+                <Text style={styles.item}>Twitter Disconnect</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.CurrentUserLogout} disabled={this.state.disableButtons}>
+              <View style={styles.itemParent}>
+                <Image style={{ height: 24, width: 25.3 }} source={loggedOutIcon} />
+                <Text style={styles.item}>Log out</Text>
+              </View>
+            </TouchableOpacity>
+          </SafeAreaView>
+        </ScrollView>
+    );
+  }
+
 };
 
 const styles = StyleSheet.create({
