9421b54855f2fe90dec68a628c0bd33ed3e5416f
Ashutosh <ashutosh@ost.com>
2019-09-03 18:49:59 +0530

    iap integration

--- AppContainer.js
+++ AppContainer.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import {Platform} from "react-native";
 import { OstWalletSdkEvents } from '@ostdotcom/ost-wallet-sdk-react-native';
 
 import RootNavigationContainer from './RootNavigationContainer';
 import Store from './src/store';
 import { Provider } from 'react-redux';
 
+import * as RNIap from 'react-native-iap';
+ 
+const itemSkus = Platform.select({
+  ios: [
+    'pepostaging1'
+  ],
+  android: [
+    'pepostaging1'
+  ]
+});
+
 export default class AppContainer extends Component {
   componentDidMount() {
     OstWalletSdkEvents.subscribeEvent();
+    try {
+      RNIap.getProducts(itemSkus).then(( products ) => {
+        console.log("Initialize in-app payment", products );
+      }).catch(( error )=> {
+        console.warn("Unable to initialize in-app payment." , error);
+      });
+    } catch(error) {
+      console.warn("Unable to initialize in-app payment." , error);
+    }
   }
 
   componentWillUnmount() {
