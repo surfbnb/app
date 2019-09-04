8ef104c0b59f1e28219b5f2b2317464c7b15bdfa
Ashutosh <ashutosh@ost.com>
2019-09-04 20:11:43 +0530

    Temp code

--- AppContainer.js
+++ AppContainer.js
@@ -6,28 +6,63 @@ import RootNavigationContainer from './RootNavigationContainer';
 import Store from './src/store';
 import { Provider } from 'react-redux';
 
-import * as RNIap from 'react-native-iap';
+import RNIap, {
+  ProductPurchase,
+  purchaseUpdatedListener,
+  purchaseErrorListener,
+} from 'react-native-iap';
  
 const itemSkus = Platform.select({
   ios: [
-    'pepostaging1'
+    'pd1'
   ],
   android: [
-    'pepostaging1'
+    'com.pepo.staging.pp123p_123', 'pp123p_123', 'pd1' , 'com.pepo.staging.pd1'
   ]
 });
 
+let purchaseUpdateSubscription , purchaseErrorSubscription;
+let allProducts ; 
+
 export default class AppContainer extends Component {
+
+
   componentDidMount() {
     OstWalletSdkEvents.subscribeEvent();
     try {
-      RNIap.getProducts(itemSkus).then(( products ) => {
-        console.log("Initialize in-app payment", products );
-      }).catch(( error )=> {
-        console.warn("Unable to initialize in-app payment." , error);
-      });
+      RNIap.initConnection().then((res)=> {
+        console.log("initConnection", res); 
+        RNIap.getProducts(itemSkus).then(( products ) => {
+          allProducts = products ;
+          console.log("Initialize in-app payment", products );
+          setImmediate(()=> {
+            this.requestPurchase( allProducts[0]["productId"] ); 
+          } , 2000 )
+        }).catch(( error )=> {
+          console.warn("Unable to initialize in-app payment." , error);
+        });
+      }).catch((error)=> {
+        console.log("initConnection", error);
+      })
+ 
     } catch(error) {
       console.warn("Unable to initialize in-app payment." , error);
+    }
+
+    purchaseUpdateSubscription = purchaseUpdatedListener(( res ) => {
+      console.log('purchaseUpdatedListener', res);
+    });
+    purchaseErrorSubscription = purchaseErrorListener((error) => {
+      console.log('purchaseErrorListener', error);
+    });
+
+  }
+
+  requestPurchase = async(sku) => {
+    try {
+      RNIap.requestPurchase(sku);
+    } catch (err) {
+      console.log("requestPurchase error" , err.code, err.message);
     }
   }
 
