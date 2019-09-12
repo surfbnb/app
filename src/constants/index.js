e9d68e8471cd8663ecddfc49a65be00868f2d043
sachinchauhan <sachin@pepo.com>
2019-09-12 15:48:33 +0530

    Reduce expiration time to 2hr in staging

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,7 +7,9 @@ const BundleConstants = {
     TRACKER_ENDPOINT: 'https://px.pepo.com/devp101_pixel.png',
     TOKEN_ID: '1185',
     TWITTER_CONSUMER_KEY: 'NEo4gEXzdQZaoTsqzpZvepfKb',
-    TWITTER_CONSUMER_SECRET: 'iM5UMt4px8rwoqEoRV9gJGrJGtEoMUxOYkaWXSges7t4bk564t'
+    TWITTER_CONSUMER_SECRET: 'iM5UMt4px8rwoqEoRV9gJGrJGtEoMUxOYkaWXSges7t4bk564t',
+    SESSION_KEY_EXPIRY_TIME: 60 * 60 * 2,
+    SPENDING_LIMIT: '1000000000000000000000'
   },
   'com.pepo.sandbox': {
     API_ROOT: 'https://sandboxpepo.com/api/v1',
@@ -33,7 +35,7 @@ export const API_ROOT = BundleConstants[DeviceInfo.getBundleId()].API_ROOT;
 export const PLATFORM_API_ENDPOINT = BundleConstants[DeviceInfo.getBundleId()].PLATFORM_API_ENDPOINT;
 export const TRACKER_ENDPOINT = BundleConstants[DeviceInfo.getBundleId()].TRACKER_ENDPOINT;
 export const TOKEN_ID = BundleConstants[DeviceInfo.getBundleId()].TOKEN_ID;
-export const SESSION_KEY_EXPIRY_TIME = 31536000;
-export const SPENDING_LIMIT = '1000000000000000000000';
+export const SESSION_KEY_EXPIRY_TIME = BundleConstants[DeviceInfo.getBundleId()].SESSION_KEY_EXPIRY_TIME || 60 * 60 * 24 * 365;
+export const SPENDING_LIMIT = BundleConstants[DeviceInfo.getBundleId()].SPENDING_LIMIT || '1000000000000000000000';
 export const TWITTER_CONSUMER_KEY = BundleConstants[DeviceInfo.getBundleId()].TWITTER_CONSUMER_KEY;
 export const TWITTER_CONSUMER_SECRET = BundleConstants[DeviceInfo.getBundleId()].TWITTER_CONSUMER_SECRET;
