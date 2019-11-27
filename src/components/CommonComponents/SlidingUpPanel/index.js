aae5be6371ede90bb6dc8b19e964852ee7818aa5
sachinchauhan <sachin@pepo.com>
2019-11-28 00:55:01 +0530

    Bug fix: back button fix

--- src/components/CommonComponents/SlidingUpPanel/index.js
+++ src/components/CommonComponents/SlidingUpPanel/index.js
@@ -2,9 +2,14 @@ import SlidingUpPanel from "rn-sliding-up-panel";
 
 class Panel extends SlidingUpPanel{
 
-    constructor(props){
-        super(props);
-    }
-} 
+  constructor(props) {
+    super(props);
+  }
+
+  _onBackButtonPress() {
+    return false;
+  }
+
+}
 
 export default Panel ;
