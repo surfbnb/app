b9d3f181f8f58904bb8ebdd2fa4df6e6ca57e8dc
Ashutosh <ashutosh@ost.com>
2019-12-10 14:34:24 +0530

    Minor fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -18,46 +18,24 @@ export const {
   upsertUserEntities,
   updateCurrentUser,
   logoutUser,
-  upsertActivitiesEntities,
-  upsertTransactionEntities,
-  upsertGiffyEntities,
-  upsertTagEntities,
   upsertLinkEntities,
   upsertUserProfileEntities,
-  upsertUserAllowedActionEntities,
-  upsertUserStatEntities,
   upsertVideoEntities,
   upsertVideoStatEntities,
   upsertReplyDetailEntities,
-  upsertCurrentUserVideoRelationEntities,
   upsertCurrentUserReplyDetailRelationEntities,
   upsertImageEntities,
-  upsertHomeFeedEntities,
   updateBalance,
   updateIsPurchase,
-  updatePricePoints,
-  updateToken,
-  updatePepocorn,
   updateExecuteTransactionStatus,
-  upsertVideoContributionEntities,
-  upsertReplyContributionEntities,
-  upsertUserContributionEntities,
   upsertRecordedVideo,
   clearRecordedVideo,
   videoInProcessing,
   upsertProfilePicture,
   clearProfilePicture,
-  upsertUserVideoEntities,
-  upsertUserContributionToStats,
-  upsertUserContributionByStats,
-  upsertUserNotifications,
-  upsertVideoDescriptionEntities,
   upsertNotificationUnread,
-  upsertPushNotification,
   clearPushNotification,
   upsertInviteCode,
-  upsertTwitterEntities,
-  upsertTagVideoEntities,
   upsertUnseenReplies,
   upsertAllWhitelisted
 } = createActions(...Object.keys(types));
@@ -153,33 +131,9 @@ export const reducer = handleActions(
       ...state,
       current_user: assignIn({}, state.current_user, action.payload.current_user)
     }),
-    [upsertActivitiesEntities]: (state, action) => ({
-      ...state,
-      activities_entities: assignIn({}, state.activities_entities, action.payload.activities_entities)
-    }),
-    [upsertTransactionEntities]: (state, action) => ({
-      ...state,
-      transaction_entities: assignIn({}, state.transaction_entities, action.payload.transaction_entities)
-    }),
-    [upsertGiffyEntities]: (state, action) => ({
-      ...state,
-      giffy_entities: assignIn({}, state.giffy_entities, action.payload.giffy_entities)
-    }),
-    [upsertTagEntities]: (state, action) => ({
-      ...state,
-      tag_entities: assignIn({}, state.tag_entities, action.payload.tag_entities)
-    }),
     [upsertUserProfileEntities]: (state, action) => ({
       ...state,
       user_profile_entities: assignIn({}, state.user_profile_entities, action.payload.user_profile_entities)
-    }),
-    [upsertUserAllowedActionEntities]: (state, action) => ({
-      ...state,
-      user_allowed_action_entities: assignIn({}, state.user_allowed_action_entities, action.payload.user_allowed_action_entities)
-    }),
-    [upsertUserStatEntities]: (state, action) => ({
-      ...state,
-      user_stat_entities: assignIn({}, state.user_stat_entities, action.payload.user_stat_entities)
     }),
     [upsertLinkEntities]: (state, action) => ({
       ...state,
@@ -197,93 +151,21 @@ export const reducer = handleActions(
       ...state,
       reply_detail_entities: assignIn({}, state.reply_detail_entities, action.payload.reply_detail_entities)
     }),
-    [upsertCurrentUserVideoRelationEntities]: (state, action) => ({
-      ...state,
-      current_user_video_relation_entities: assignIn({}, state.current_user_video_relation_entities, action.payload.current_user_video_relation_entities)
-    }),
     [upsertCurrentUserReplyDetailRelationEntities]: (state, action) => ({
       ...state,
       current_user_reply_detail_relation_entities: assignIn({}, state.current_user_reply_detail_relation_entities, action.payload.current_user_reply_detail_relation_entities)
-    }),
-    [upsertVideoDescriptionEntities]: (state, action) => ({
-      ...state,
-      video_description_entities: assignIn(
-        {},
-        state.video_description_entities,
-        action.payload.video_description_entities
-      )
     }),
     [upsertImageEntities]: (state, action) => ({
       ...state,
       image_entities: assignIn({}, state.image_entities, action.payload.image_entities)
     }),
-    [upsertHomeFeedEntities]: (state, action) => ({
-      ...state,
-      home_feed_entities: assignIn({}, state.home_feed_entities, action.payload.home_feed_entities)
-    }),
-    [upsertUserVideoEntities]: (state, action) => ({
-      ...state,
-      user_video_entities: assignIn({}, state.user_video_entities, action.payload.user_video_entities)
-    }),
-    [upsertTagVideoEntities]: (state, action) => ({
-      ...state,
-      tag_video_entities: assignIn({}, state.tag_video_entities, action.payload.tag_video_entities)
-    }),
-    [upsertVideoContributionEntities]: (state, action) => ({
-      ...state,
-      video_contribution_entities: assignIn(
-        {},
-        state.video_contribution_entities,
-        action.payload.video_contribution_entities
-      )
-    }),
-    [upsertReplyContributionEntities]: (state, action) => ({
-      ...state,
-      reply_contribution_entities: assignIn(
-        {},
-        state.reply_contribution_entities,
-        action.payload.reply_contribution_entities
-      )
-    }),
-    [upsertUserContributionEntities]: (state, action) => ({
-      ...state,
-      user_contribution_entities: assignIn(
-        {},
-        state.user_contribution_entities,
-        action.payload.user_contribution_entities
-      )
-    }),
     [updateBalance]: (state, action) => ({
       ...state,
       balance: action.payload.balance
-    }),
+    }), 
     [updateIsPurchase]: (state, action)=>({
       ...state,
       balance: action.payload.isPurchase
-    }),
-    [updatePricePoints]: (state, action) => {
-      //Make sure price_points is not null;
-      if (!action.payload.price_points) {
-        return {...state};
-      }
-
-      // Make sure response has keys;
-      if ( !Object.keys(action.payload.price_points).length ) {
-        return {...state};
-      }
-
-      return {
-        ...state,
-        price_points: action.payload.price_points
-      };
-    },
-    [updateToken]: (state, action) => ({
-      ...state,
-      token: action.payload.token
-    }),
-    [updatePepocorn]: (state, action) => ({
-      ...state,
-      pepocorn: action.payload.pepocorn
     }),
     [updateExecuteTransactionStatus]: (state, action) => ({
       ...state,
@@ -316,22 +198,6 @@ export const reducer = handleActions(
       ...state,
       ...logoutDefault
     }),
-    [upsertUserContributionByStats]: (state, action) => ({
-      ...state,
-      user_contribution_by_stats: merge({}, state.user_contribution_by_stats, action.payload.user_contribution_by_stats)
-    }),
-    [upsertUserContributionToStats]: (state, action) => ({
-      ...state,
-      user_contribution_to_stats: merge({}, state.user_contribution_to_stats, action.payload.user_contribution_to_stats)
-    }),
-    [upsertUserNotifications]: (state, action) => ({
-      ...state,
-      user_notifications: assignIn({}, state.user_notifications, action.payload.user_notifications)
-    }),
-    [upsertPushNotification]: (state, action) => ({
-      ...state,
-      push_notification: assignIn({}, state.push_notification, action.payload.push_notification)
-    }),
     [clearPushNotification]: (state, action) => ({
       ...state,
       push_notification: assignIn(defaultState.push_notification)
@@ -343,10 +209,6 @@ export const reducer = handleActions(
     [upsertInviteCode]: (state, action) => ({
       ...state,
       invite_code: action.payload.invite_code
-    }),
-    [upsertTwitterEntities]: (state, action) => ({
-      ...state,
-      twitter_entities: assignIn({}, state.twitter_entities, action.payload.twitter_entities)
     }),
     [upsertUnseenReplies]: (state, action) => ({
       ...state,
