8878757601ba580051a0a6b4dd03d3689f1d7966
Mayur Patil <mayurupatil@gmail.com>
2020-01-28 15:26:37 +0530

    fix

--- src/components/CommonComponents/PinnedLabel/index.js
+++ src/components/CommonComponents/PinnedLabel/index.js
@@ -4,10 +4,12 @@ import reply_video from '../../../assets/video-reply.png';
 import inlineStyle from "./styles";
 
 export default (props) => {
-    return props.isPinned && (
-        <View style={inlineStyle.wrapper}>
-            <Image source={reply_video} style={inlineStyles.image} /> 
-            <Text style={inlineStyles.text}>Pinned Video</Text>
-        </View> 
-)};
+    if( props.isPinned ){
+      return   <View style={inlineStyle.wrapper}>
+        <Image source={reply_video} style={inlineStyles.image} />
+        <Text style={inlineStyles.text}>Pinned Video</Text>
+      </View>
+    }
+    return  null;
+};
 
