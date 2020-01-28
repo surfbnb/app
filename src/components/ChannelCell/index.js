7619be361d3e4f73327113f9c257e1fa4bf6dd93
thahir-reddy <thahir@ost.com>
2020-01-28 16:12:08 +0530

    channel cell UI

--- src/components/ChannelCell/index.js
+++ src/components/ChannelCell/index.js
@@ -35,16 +35,20 @@ class ChannelCell extends PureComponent {
   render() {
     return <View style={styles.channelCellWrapper}>
       <ImageBackground source={ {uri: this.props.backgroundImgUrl} } style={{width: '100%', aspectRatio: 21/9}}  resizeMode={'cover'}>
-        <View style={{padding: 12}}>
-      <Text style={styles.header}>{this.props.channelName} </Text>
-      <Text style={styles.channelDesc}>{this.props.channelTagLine}</Text>
-      <View style={styles.bottomView}>
-        <Text style={styles.memberText}>{this.props.channelUserCount} Members</Text>
-        <Text style={styles.videoText}>{this.props.channelVideoCount} Videos</Text>
-        <View style={styles.joinViewWrapper}>
-          {this.joined()}
-        </View>
-      </View>
+        <View style={{flex: 1, padding: 12, justifyContent: 'space-between', backgroundColor: 'rgba(0, 0, 0, 0.5)'}}>
+          <View>
+            <Text style={styles.header}>{this.props.channelName} </Text>
+            <Text style={styles.channelDesc}>{this.props.channelTagLine}</Text>
+          </View>
+          <View style={styles.bottomView}>
+            <View style={{flexDirection: 'row', flex: 3}}>
+              <Text style={styles.memberText}>{this.props.channelUserCount} Members</Text>
+              <Text style={styles.videoText}>{this.props.channelVideoCount} Videos</Text>
+            </View>
+            <View style={styles.joinViewWrapper}>
+              {this.joined()}
+            </View>
+          </View>
         </View>
       </ImageBackground>
     </View>
