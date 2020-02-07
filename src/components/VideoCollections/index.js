1e9a966a4349230f42083347e6a56967c62c914a
Shraddha <shraddha.patil@ost.com>
2020-02-07 20:03:22 +0530

    silent update another flag

--- src/components/VideoCollections/index.js
+++ src/components/VideoCollections/index.js
@@ -54,9 +54,15 @@ class VideoCollections extends PureComponent {
         let fetchUrl = this.props.getFetchUrl();
         let params = this.props.getFetchParams();
         this.videoPagination = new Pagination(fetchUrl , null , null ,params);
-        this.state.loadingNext = false;
+        this.silenUpdateState();
+
         this.bindPaginationEvents();
     }
+
+  silenUpdateState(){
+    this.state.loadingNext = false;
+    this.state.refreshing = false;
+  }
 
     bindPaginationEvents(){
         let pagination = this.videoPagination;
