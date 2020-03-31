be01798ee64fde2b7ece16401037875144f93ba0
Ashutosh <ashutosh@ost.com>
2020-03-31 20:00:17 +0530

    CoachScreen refactor

--- src/components/VideoRecorder/index.js
+++ src/components/VideoRecorder/index.js
@@ -33,6 +33,7 @@ import Utilities from '../../services/Utilities';
 import FfmpegProcesser from '../../services/FfmpegProcesser';
 import ProgressBar from '../CommonComponents/ProgressBarWrapper';
 import VideoLength from './VideoLength';
+import CoachScreen from './CoachScreen';
 
 const ACTION_SHEET_BUTTONS = ['Reshoot', 'Continue'];
 const ACTION_SHEET_CONTINUE_INDEX = 1;
@@ -254,87 +255,15 @@ class VideoRecorder extends Component {
 
 
   showCoachForPosting = () => {
-    // If already recorded video present in local, do not show coach.
-    if (this.state.isLocalVideoPresent) return;
-
-    if (this.props.isVideoTypeReply) {
-      // TODO: return coach for posting
-      if (this.state.showLightBoxOnReply){
-        // Show video
-        return <View style={styles.backgroundStyle}>
-          <TouchableOpacity onPressIn={this.cancleVideoHandling} style={[styles.closeBtWrapper, {top: 25}]}>
-            <Image style={{height: 16.6, width: 16.6}} source={closeIconWhite}></Image>
-          </TouchableOpacity>
-          <View style={{ padding: 26, alignItems: 'center'}}>
-
-            <Text style={[styles.smallText, {fontWeight: '600'}]}>
-              Post a reply
-            </Text>
-
-
-            <Text style={[styles.miniText, {textAlign: 'center'}]}>
-              Reply to {this.getUserFullName()}'s video for {this.getPepoAmount()} Pepo Coins
-            </Text>
-
-            <LinearGradient
-              colors={['#ff7499', '#ff5566']}
-              locations={[0, 1]}
-              start={{ x: 0, y: 0 }}
-              end={{ x: 1, y: 0 }}
-              style={{ alignSelf: 'center', paddingHorizontal: 15, marginTop: 30, borderRadius: 3 }}
-            >
-              <TouchableButton
-                TouchableStyles={[{ minWidth: '100%', borderColor: 'none', borderWidth: 0 }]}
-                TextStyles={[Theme.Button.btnPinkText]}
-                style={{marginBottom: 20}}
-                textBeforeImage='Reply | '
-                textAfterImage={this.getPepoAmount()}
-                onPress={multipleClickHandler(() => {
-                  this.replyToVideo();
-                })}
-              />
-            </LinearGradient>
-
-          </View>
-        </View>
-      }
-      // if (no video reply present) { return Coach }
-    } else {
-      if (this.state.acceptedCameraTnC !== 'true') {
-
-        return <View style={styles.backgroundStyle}>
-          <View style={{ padding: 26 }}>
-            <Text style={styles.headerText}>Create your first 30 second video</Text>
-
-            <Text style={styles.smallText}>
-              Introduce yourself. Share your passions, projects, and your unique personality!
-            </Text>
-
-            <LinearGradient
-              colors={['#ff7499', '#ff5566']}
-              locations={[0, 1]}
-              start={{ x: 0, y: 0 }}
-              end={{ x: 1, y: 0 }}
-              style={{ alignSelf: 'center', paddingHorizontal: 15, marginTop: 30, borderRadius: 3 }}
-            >
-              <TouchableOpacity
-                onPress={this.acceptCameraTerms}
-                style={[Theme.Button.btn, { borderWidth: 0 }]}
-              >
-                <Text style={[
-                  Theme.Button.btnPinkText,
-                  { fontSize: 16, fontFamily: 'AvenirNext-DemiBold', textAlign: 'center' }
-                ]}>
-                  Start Creating
-                </Text>
-              </TouchableOpacity>
-            </LinearGradient>
-
-          </View>
-        </View>
-
-      }
-    }
+    return <CoachScreen videoId={this.props.videoId}
+                        isLocalVideoPresent={this.state.isLocalVideoPresent}
+                        acceptedCameraTnC={this.state.acceptedCameraTnC}
+                        acceptCameraTerms={this.acceptCameraTerms}
+                        isVideoTypeReply={this.props.isVideoTypeReply}
+                        showLightBoxOnReply={this.state.showLightBoxOnReply}
+                        cancleVideoHandling={this.cancleVideoHandling}
+                        replyToVideo={this.replyToVideo}
+                        />
   };
 
   cameraView() {
